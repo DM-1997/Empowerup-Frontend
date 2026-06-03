c3ecbdfc05cdcb6fbba0eee3f19129a545d7515d
DM-1997 <benjamim.cahando6@gmail.com>
2026-06-03 10:37:46 +0100

    Nova02

--- src/app/pages/support-campaign/support-campaign.ts
+++ src/app/pages/support-campaign/support-campaign.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { BehaviorSubject, switchMap, map } from 'rxjs';
 import { CampaignService } from '../../core/services/campaign.service';
 
 @Component({
@@ -11,36 +12,49 @@ import { CampaignService } from '../../core/services/campaign.service';
   templateUrl: './support-campaign.html',
   styleUrl: './support-campaign.css',
 })
-export class SupportCampaign implements OnInit {
+export class SupportCampaign {
 
-  campaign: any;
   valor: number = 0;
+
+  private campaignId: number;
+
+  private refresh$ = new BehaviorSubject<void>(undefined);
+
+  campaign$ = this.refresh$.pipe(
+    switchMap(() =>
+      this.campaignService.getAllCampaigns()
+    ),
+    map(campaigns =>
+      campaigns.find(c => c.id === this.campaignId)
+    )
+  );
 
   constructor(
     private route: ActivatedRoute,
     private campaignService: CampaignService
-  ) {}
-
-  ngOnInit(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-
-    this.loadCampaign(id);
-  }
-
-  loadCampaign(id: number) {
-    this.campaignService.getAllCampaigns()
-      .subscribe(data => {
-        this.campaign = data.find(c => c.id === id);
-      });
+  ) {
+    this.campaignId = Number(
+      this.route.snapshot.paramMap.get('id')
+    );
   }
 
   apoiar(): void {
-    if (!this.campaign || this.valor <= 0) return;
 
-    this.campaignService.supportCampaign(this.campaign.id, this.valor)
-      .subscribe(updated => {
-        this.campaign = updated;
-        this.valor = 0;
+    if (this.valor <= 0) {
+      return;
+    }
+
+    this.campaignService
+      .supportCampaign(this.campaignId, this.valor)
+      .subscribe({
+        next: () => {
+
+          this.valor = 0;
+
+          // 🔥 recarrega os dados da campanha
+          this.refresh$.next();
+        },
+        error: err => console.error(err)
       });
   }
 }
